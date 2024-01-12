aca3e6baf80952103609984b1ad75b778128e3f7
umesh <umesh.patidar@mangoitsolutions.in>
2024-01-12 11:33:24 +0530

    passport package installation and setup with Api controller creation

---
+++ app/Http/Controllers/api/ApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    // Register API (post)
+    public function register(Request $request) {
+
+    }
+
+    //  Login API (post)
+    public function login(Request $request) {
+
+    }
+
+    // Profile API (get)
+    public function profile() {
+
+    }
+
+    //  Logout API (get)
+    public function logout() {
+
+    }
+}
